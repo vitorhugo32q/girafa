7f4476041adc050c93706abed8b245f142d0d4f6
vitorhugo32q <vitorhugo1s2r@gmail.com>
2024-11-25 10:07:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'biologia',
+    'qual é a familia que o lobo guará participar?',
+    'Ele participa da familia Canidae que é uma família de mamíferos da ordem Carnivora que engloba cães, lobos, chacais, coiotes e raposas. É composto por 36 espécies distribuídas por todos os continentes com exceção da Antártica.'
+)
+
+criaCartao(
+    'biologia',
+    'Como se chama a pessoa que estuda os insetos?',
+    'Entomologia é a especialidade da biologia que estuda os insetos sob todos os seus aspectos e relações com o homem, as plantas, os animais e o meio ambiente.'
+)
+
+criaCartao(
+    'biologia',
+    ' Me explique como fuciona uma teia alimentar?',
+    'A teia alimentar é um conjunto de cadeias alimentares interligadas. Existem três níveis tróficos na cadeia alimentar: produtores, consumidores e decompositores.'
+)
+
+criaCartao(
+    'biologia',
+    'Como é feita a teia de uma aranha?',
+    'As teias são feitas de uma seda composta por proteínas. Na maioria dos casos, a estrutura é usada para a captura de presas, como os insetos, que servem de alimento para as aranhas'
+)
